2f42cc6de5845d75ff00db5f24d87fa24ed50f0f
Oveln <oveln@outlook.com>
2023-06-01 15:26:03 +0800

    动画

--- src/components/TeacherList/TeacherCard.vue
+++ src/components/TeacherList/TeacherCard.vue
@@ -2,18 +2,26 @@
 <div class="ov-teachercard">
     <div class="ov-teachercard__up">
         <img class="ov-teacher__up--avatar" :src="avt_url.toString()" alt="">
-        <span @click="cilck">{{ name }}</span>
+        <span>{{ name }}</span>
+        <div class="zhanwei">&nbsp</div>
+        <button @click="cilck">展开课程</button>
     </div>
     <div class="ov-teachercard__down">
-        {{ description }}
+        教师介绍：{{ description }}
+        <Transition>
+            <div v-show="lesson_show" class="ov-teachercard__down--lesson">
+                课程列表
+            </div>
+        </Transition>
     </div>
 </div>
 </template>
 
 <script setup lang='ts'>
+import {ref} from 'vue'
 export interface TeacherData {
     name: String,
-    avt_url: String,
+    avt_url?: String,
     description: String
 }
 let props = withDefaults(
@@ -21,12 +29,26 @@ let props = withDefaults(
         avt_url: ()=> 'src/assets/face.jpg'
     }
 )
-const cilck = ()=>{console.log(props.avt_url.toString());}
+let lesson_show = ref(false)
+const cilck = ()=>{console.log(props.avt_url.toString());lesson_show.value=!lesson_show.value;}
 </script>
 <style lang='scss' scoped>
-:hover {
-    background-color: #ccc;
+// :hover {
+//     backgrou nd-color: #ccc;
+// }
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+.v-enter-to,
+.v-leave-from {
+  opacity: 1;
+}
+
 @include b(teachercard) {
     display: flex;
     flex-direction:column;
@@ -34,6 +56,7 @@ const cilck = ()=>{console.log(props.avt_url.toString());}
     margin: 15px;
     border-radius: 5px;
     border: 1px solid #ccc;
+    box-shadow: 5px 8px 10px #ccc;
     @include e(up) {
         flex: 1;
         display: flex;
@@ -47,11 +70,21 @@ const cilck = ()=>{console.log(props.avt_url.toString());}
         span{
             margin: 0px 10px;
         }
+        .zhanwei {flex:1;}
         border-bottom: 1px solid #ccc;
     }
     @include e(down) {
+        display: flex;
+        flex-direction: column;
         flex: 3;
-        margin: 10px 5px;
+        // margin: 10px 5px;
+        @include m(lesson) {
+            margin-top: 10px;
+            padding: 0;
+            width: 100%;
+            border-top: 1px solid #ccc;
+            flex: 1;
+        }
     }
 }
 </style>
